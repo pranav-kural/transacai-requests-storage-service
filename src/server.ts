c16db046043349d45817c67573d55d545cb58b37
pranav-kural <pranavkural042@gmail.com>
2024-10-26 17:08:06 -0400

    Server setup with schema and context

--- src/server.ts
+++ src/server.ts
@@ -3,19 +3,23 @@ import { startStandaloneServer } from '@apollo/server/standalone'
 import { schema } from './schema'
 import { Context, createContext } from './context'
 
+// Method to start the server
 const start = async () => {
-  const server = new ApolloServer<Context>({ schema })
+  // Create a new Apollo Server instance using the schema and context
+  const server = new ApolloServer<Context>({
+    schema,
+    introspection: process.env.NODE_ENV !== 'production', // Disable introspection in production
+  })
 
+  // Start standalone server with createContext function
   const { url } = await startStandaloneServer(server, {
     context: createContext,
-    listen: { port: 4000 }
+    listen: { port: 4000 },
   })
 
   console.log(`\
-  🚀 Server ready at: ${url}
-  ⭐️ See sample queries: http://pris.ly/e/ts/graphql-nexus#using-the-graphql-api
-  `)
-
+  🚀 Transac AI ISS listening at: ${url}`)
 }
 
+// start on load
 start()
