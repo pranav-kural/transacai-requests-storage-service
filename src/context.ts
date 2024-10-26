65be9553c5ceac6c39ee3e685f7235dca75923e8
pranav-kural <pranavkural042@gmail.com>
2024-10-26 16:54:39 -0400

    Update server context to authenticate API key for ISS

--- src/context.ts
+++ src/context.ts
@@ -1,4 +1,6 @@
 import { PrismaClient } from '@prisma/client'
+import { GraphQLError } from 'graphql'
+import type { IncomingMessage, ServerResponse } from 'http'
 
 export interface Context {
   prisma: PrismaClient
@@ -6,6 +8,27 @@ export interface Context {
 
 const prisma = new PrismaClient()
 
-export const createContext = async () => ({
-  prisma: prisma,
-})
+export const createContext = async ({
+  req,
+}: {
+  req: IncomingMessage
+  res: ServerResponse
+}) => {
+  // Get API key from headers
+  const apiKey = req.headers.authorization || ''
+
+  // Verify API key
+  if (apiKey !== process.env.TRANSAC_AI_ISS_API_KEY) {
+    // if the API key is invalid, return status 401
+    throw new GraphQLError('Invalid API key', {
+      extensions: {
+        code: 'UNAUTHENTICATED',
+        http: { status: 401 },
+      },
+    })
+  }
+
+  return {
+    prisma: prisma,
+  }
+}
